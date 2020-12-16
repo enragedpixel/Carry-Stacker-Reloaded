eafe823866b45bc3a0f795f38ec9788bd5035846
m-alorda <mail@m-alorda.com>
2020-12-16 21:36:42 +0100

    Removed unnecessary antianticheat methods
    This mod is not changing the way equipment or grenades are handled.
    Hence, it is not necessary to trick the anticheat.

--- lua/antianticheat.lua
+++ lua/antianticheat.lua
@@ -29,42 +29,6 @@ function PlayerManager:verify_carry(peer, carry_id)
 end
 
 --[[
-	This function will be called whenever the player's equipment is to be
-	verified.
-
-	If false is returned, the anticheat will detect cheating.
-]]
-local master_PlayerManager_verify_equipment = PlayerManager.verify_equipment
-function PlayerManager:verify_equipment(peer, equipment_id)
-	BLT_CarryStacker:Log("Request to verify equipment")
-	return doAntiAntiCheat(master_PlayerManager_verify_equipment, self, peer, carry_id)
-end
-
---[[
-	This function will be called whenever the player's granade is to be
-	verified.
-
-	If false is returned, the anticheat will detect cheating.
-]]
-local master_PlayerManager_verify_grenade = PlayerManager.verify_grenade
-function PlayerManager:verify_grenade(peer)
-	BLT_CarryStacker:Log("Request to verify granade")
-	return doAntiAntiCheat(master_PlayerManager_verify_grenade, self, peer)
-end	
-
---[[
-	This function will be called whenever the player gets a new 
-	granade.
-
-	If false is returned, the anticheat will detect cheating.
-]]
-local master_PlayerManager_register_grenade = PlayerManager.register_grenade
-function PlayerManager:register_grenade(peer)
-	BLT_CarryStacker:Log("Request to register granade")
-	return doAntiAntiCheat(master_PlayerManager_register_grenade, self, peer)
-end
-
---[[
 	This function will be called whenever the player gets a new carry.
 
 	If false is returned, the anticheat will detect cheating.
